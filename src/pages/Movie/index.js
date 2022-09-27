93db6599ed06be0e0c4dcedac802cb31ca7cf8a8
Andre Bauer <andrebauer@live.com>
2022-09-27 12:17:16 +0900

    making smooth navigation

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from "../../services/api";
+
 function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadMovie() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "5bebc0d87ffec1997ff48dbbc3bc98ce",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setMovie(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("MOVIE NOT FOUND");
+        });
+    }
+    loadMovie();
+
+    return () => {
+      console.log("Componente desmontado");
+    };
+  }, []);
+
   return (
     <div>
-      <h1>Welcome to the movie details page</h1>
+      <h1>Acessando o filme {id}</h1>
     </div>
   );
 }
