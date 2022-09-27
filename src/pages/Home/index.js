c7980ea90cf9c539c6147ea9db38f924f5d8a806
Andre Bauer <andrebauer@live.com>
2022-09-27 11:02:23 +0900

    cards and list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
+import "./home.css";
 
 function Home() {
   const [movies, setMovies] = useState([]);
@@ -13,14 +15,28 @@ function Home() {
           page: 1,
         },
       });
-      console.log(response.data);
+      //console.log(response.data.results.slice(0, 10));
+      setMovies(response.data.results.slice(0, 10));
     }
     loadMovies();
   }, []);
 
   return (
-    <div>
-      <h1>Welcome to the Homepage</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {movies.map((movie) => {
+          return (
+            <article key={movie.id}>
+              <strong>{movie.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <Link to={`/movie/${movie.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
