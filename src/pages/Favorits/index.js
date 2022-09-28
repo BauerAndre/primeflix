66c8b3a41eb484cfeea2154620e23277f0ef6c44
Andre Bauer <andrebauer@live.com>
2022-09-28 11:39:51 +0900

    favorits view and route

---
+++ src/pages/Favorits/index.js
@@ -0,0 +1,11 @@
+import "./favorits.css";
+
+function Favorits() {
+  return (
+    <div>
+      <h1>Favorits screen</h1>
+    </div>
+  );
+}
+
+export default Favorits;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Movie from "./pages/Movie";
+import Favorits from "./pages/Favorits";
 
 import Error from "./pages/Error";
 
@@ -14,6 +15,7 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<Movie />} />
+        <Route path="favorits" element={<Favorits />} />
 
         <Route path="*" element={<Error />} />
       </Routes>
