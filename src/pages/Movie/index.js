5f67bbd87de881de52552818bc4ac64f44eb9674
Andre Bauer <andrebauer@live.com>
2022-09-28 10:59:32 +0900

    show page with style

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import "./movie-info.css";
 import api from "../../services/api";
 
 function Movie() {
@@ -51,6 +51,13 @@ function Movie() {
       <span>{movie.overview}</span>
 
       <strong>Rating: {movie.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+        <button>Save</button>
+        <button>
+          <a href="#">Trailer</a>
+        </button>
+      </div>
     </div>
   );
 }
